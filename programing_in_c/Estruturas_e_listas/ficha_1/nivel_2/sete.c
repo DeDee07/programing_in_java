6c21af0dc742e88b574449acb17a468a722fb4b5
Dede07 <154218049+DeDee07@users.noreply.github.com>
2025-09-29 13:25:13 +0100

    Projectsschool

---
+++ programing_in_c/Estruturas_e_listas/ficha_1/nivel_2/sete.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Aluno {
+    float media;
+    char* nome;
+    int numero;
+};
+
+int main() {
+    struct Aluno a1 = {8.75, "João Silva", 1};
+    struct Aluno a2 = {10.95, "Leandro Oliveira", 2};
+
+    printf("O nome é %s e número é %d e a sua média é %.2f\n", a1.nome, a1.numero, a1.media);
+    printf("O nome é %s e número é %d e a sua média é %.2f\n", a2.nome, a2.numero, a2.media);
+
+    return 0;
+}
