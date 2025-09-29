1789443dc6fbc52c5bc33d6fd445dadaf72ed431
Dede07 <154218049+DeDee07@users.noreply.github.com>
2025-09-29 19:27:20 +0100

    Learning

---
+++ Learning_in_house/learning_java/basic/nivel_1/OlaMundo.java
@@ -0,0 +1,7 @@
+package learning_java.basic.nivel_1;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Learning_in_house/learning_java/basic/nivel_1/maioridade.java
@@ -0,0 +1,13 @@
+package learning_java.basic.nivel_1;
+
+public class maioridade {
+    public static void main(String[] args) {
+        int idade = 20;
+        
+        if (idade >= 18) {
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
